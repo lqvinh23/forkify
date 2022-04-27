fab2b7432d6e37a598a08cc977a428253e2441a9
lqvinh23 <galacticos2cr7@gmail.com>
2022-04-27 14:40:34 +0700

    New feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,6 +118,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -126,6 +130,6 @@ const init = function () {
   searchView.addHandlerSeach(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
